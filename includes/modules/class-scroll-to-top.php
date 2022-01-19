8cd7196f309105842e4b4b72303c53f4996cca4b
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-01-19 17:43:23 +0100

    updated Scroll to Top button script to use vanilla js

--- includes/modules/class-scroll-to-top.php
+++ includes/modules/class-scroll-to-top.php
@@ -49,7 +49,7 @@ class Treville_Pro_Scroll_To_Top {
 		// Call Credit Link function of theme if credit link is activated.
 		if ( true === $theme_options['scroll_to_top'] && ! self::is_amp() ) :
 
-			wp_enqueue_script( 'treville-pro-scroll-to-top', TREVILLE_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array( 'jquery' ), '20210603', true );
+			wp_enqueue_script( 'treville-pro-scroll-to-top', TREVILLE_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array(), '20220119', true );
 
 			// Passing Parameters to navigation.js.
 			wp_localize_script( 'treville-pro-scroll-to-top', 'trevilleProScrollToTop', array( 'icon' => self::get_svg( 'collapse' ) ) );
